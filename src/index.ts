8930931b26536a73bd9661575b6928388c73cefb
Josh Gachnang <josh@nang.io>
2022-07-27 14:53:12 -0500

    Export plugins (#78)
    When moving code around, I forgot to add plugins to index.ts so they'd be
    exported outside the package.

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,6 @@ export * from "./expressServer";
 export * from "./logger";
 export * from "./passport";
 export * from "./permissions";
+export * from "./plugins";
 export * from "./transformers";
 export * from "./utils";
