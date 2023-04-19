8dd63fa30fa229692bb22dd11d27cf00aa5e7067
Kyle Kurtz <kyle@kurtz.io>
2023-04-19 10:52:31 -0500

    Add test as NODE_ENV option

--- types.d.ts
+++ types.d.ts
@@ -1,6 +1,6 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
-    NODE_ENV: "development" | "production";
+    NODE_ENV: "development" | "production" | "test";
     LOGGING_LEVEL: "debug" | "info" | "warn" | "error";
     SENTRY_DSN: string;
     SENTRY_TRACES_SAMPLE_RATE: string;
